99cb961e83805d0885d6506ccb71149efb838895
YauheniRusinchuk <ruevgal@gmail.com>
2019-05-03 08:36:47 +0300

    add logout btn

--- config/settings.py
+++ config/settings.py
@@ -113,6 +113,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGOUT_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('src.apps.home.urls', namespace='home')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
