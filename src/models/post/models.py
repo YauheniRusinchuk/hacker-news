e2f56dba5f2065af302bd6aba84742f4dd591f01
YauheniRusinchuk <ruevgal@gmail.com>
2019-05-04 16:02:50 +0300

    add btn add new post

--- src/models/post/models.py
+++ src/models/post/models.py
@@ -3,9 +3,13 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    title   = models.CharField(max_length=300, blank=False)
-    text    = models.TextField(blank=True)
-    author  = models.ForeignKey(User, on_delete=models.CASCADE)
+    title       = models.CharField(max_length=300, blank=False)
+    text        = models.TextField(blank=True)
+    author      = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-create_time']
 
 
     def __str__(self):
