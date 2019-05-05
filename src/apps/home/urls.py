00df7f6370f84e009341a90e2b53e58d14ac2776
YauheniRusinchuk <ruevgal@gmail.com>
2019-05-05 14:33:30 +0300

    add style

--- src/apps/home/urls.py
+++ src/apps/home/urls.py
@@ -14,7 +14,7 @@ app_name = 'home'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home_page'),
-    path('article/<int:pk>/', DetailView.as_view(), name='detail_page'),
+    path('article<int:pk>/', DetailView.as_view(), name='detail_page'),
     path('new/', NewView.as_view(), name='new_page'),
     path('login/', LoginView.as_view(), name='login_page'),
     path('register/', RegisterView.as_view(), name='register_page'),
