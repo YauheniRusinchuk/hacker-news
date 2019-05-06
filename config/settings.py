bab30fb0a628256adf3f6716f197c788a3ec3689
YauheniRusinchuk <ruevgal@gmail.com>
2019-05-06 09:08:30 +0300

    change url

--- config/settings.py
+++ config/settings.py
@@ -115,6 +115,7 @@ USE_L10N = True
 USE_TZ = True
 
 LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home:login_page'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
--- src/apps/home/urls.py
+++ src/apps/home/urls.py
@@ -15,7 +15,7 @@ app_name = 'home'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home_page'),
-    path('create/', CreateView.as_view(), name='create_page'),
+    path('article/create/', CreateView.as_view(), name='create_page'),
     path('article<int:pk>/', DetailView.as_view(), name='detail_page'),
     path('new/', NewView.as_view(), name='new_page'),
     path('login/', LoginView.as_view(), name='login_page'),
